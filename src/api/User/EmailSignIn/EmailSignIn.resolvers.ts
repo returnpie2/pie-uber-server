5612bd6a8f43b73bc1e3d9c851dd7b128cfad476
mistycoders <mistycoders@gmail.com>
2019-12-22 18:12:48 +0900

    Sign In with Email

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -0,0 +1,49 @@
+import { Resolvers } from "src/types/resolvers";
+import {
+  EmailSignInMutationArgs,
+  EmailSignInResponse
+} from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email, password } = args;
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: "No User found with that email",
+            token: null
+          };
+        }
+        const checkPassword = await user.comparePassword(password as string);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: "coming soon"
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Wrong password",
+            token: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
