8e948bcfaac0ae011ab036c6c8b037308c1f3646
mistycoders <mistycoders@gmail.com>
2020-01-02 22:41:20 +0900

    get nearby drivers

---
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -0,0 +1,36 @@
+import { Resolvers } from "src/types/resolvers";
+import privateResolver from "src/utils/privateResolver";
+import User from "src/entities/User";
+import { GetNearbyDriversResponse } from "src/types/graph";
+import { Between, getRepository } from "typeorm";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetNearbyDrivers: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
+        const user: User = req.user;
+        const { lastLat, lastLng } = user;
+        try {
+            const drivers: any = await getRepository(User).find({
+                isDriving: true,
+                lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+                lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+              });
+              return {
+                  ok: true,
+                  error: null,
+                  drivers
+              }
+        } catch (error) {
+            return {
+                ok: false,
+                error: error.message,
+                drivers: null
+            }
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
