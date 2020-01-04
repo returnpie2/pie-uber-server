c63d7fd5ca5c688218c96982fa2b2a1fcc504c53
mistycoders <mistycoders@gmail.com>
2020-01-05 02:17:04 +0900

    drivers subscription

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import { NextFunction, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
@@ -8,12 +8,16 @@ import decodeJWT from "./utils/decodeJWT";
 
 class App {
   public app: GraphQLServer;
+  public pubSub: any;
   constructor() {
+    this.pubSub = new PubSub();
+    this.pubSub.ee.setMaxListeners(99);
     this.app = new GraphQLServer({
       schema,
       context: req => {
         return {
-          req: req.request
+          req: req.request,
+          pubSub: this.pubSub
         };
       }
     });
