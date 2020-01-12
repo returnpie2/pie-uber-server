d50ce0d7f484124a01a70ade65ffe6b501ed3d74
mistycoders <mistycoders@gmail.com>
2020-01-12 13:00:46 +0900

    message entity

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -18,6 +18,9 @@ import User from "./User";
     @Column({type: "text"})
     text: string;
 
+    @Column({nullable: true})
+    chatId: number;
+
     @ManyToOne(type =>Chat, chat => chat.messages)
     chat: Chat;
 
